89f9b4cee37e921b55a25eac6f01f96b7855822e
unknown <laxmanmanandhar7@gmail.com>
2019-12-08 10:58:16 +0545

    nepalehr merge into 92

--- ui/app/registration/controllers/visitController.js
+++ ui/app/registration/controllers/visitController.js
@@ -20,6 +20,9 @@ angular.module('bahmni.registration')
                     $scope.patient = openmrsPatientMapper.map(openMRSPatient);
                     $scope.patient.name = openMRSPatient.patient.person.names[0].display;
                     $scope.patient.uuid = openMRSPatient.patient.uuid;
+                    if ($scope.patient['NHIS Number'] != null) {
+                        isMemberEligible($scope.patient['NHIS Number']);
+                    }
                 });
                 return deferred.promise;
             };
@@ -324,4 +327,11 @@ angular.module('bahmni.registration')
                         getConceptSet();
                     });
                 }));
+            var isMemberEligible = function (nhisNumber) {
+                visitService.isEligible(nhisNumber).then(function (response) {
+                    console.log("response in controller");
+                    console.log(response);
+                    $scope.eligibleData = response.data.eligibilityBalance;
+                });
+            };
         }]);
